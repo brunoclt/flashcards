10ef4b6622d8257c4b664a1f03629cf771718e80
Bruno Terceti <148259775+brunoclt@users.noreply.github.com>
2024-12-04 11:52:32 -0300

    Add files via upload

---
+++ ProjetoAlura3/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é C++?',
+    'O C++ é uma linguagem de programação'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz carro em Inglês?',
+    'car'
+)
